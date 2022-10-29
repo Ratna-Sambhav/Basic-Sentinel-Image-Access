52177f60921305e010440fec91e27f16de1dbf8b
Ratna Sambhav <72562461+Ratna-Sambhav@users.noreply.github.com>
2022-10-29 21:03:13 +0530

    Delete DS1_Get_Image directory

--- DS1_Get_Image/App_pystac/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import main, dataDetail, list
-
-urlpatterns = [
-    path('', main.as_view(), name='main'),
-    path('redirected_detail/<int:pk>/', dataDetail.as_view(), name='listDetail'),
-    path('allEntries', list.as_view(), name='allEntries'),
-] 
--- DS1_Get_Image/get_image_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import satellite_image
-
-urlpatterns = [
-    path('', satellite_image, name='satellite_image')
-] 
